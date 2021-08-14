570c6599b887f98e09bc6b435719c4b5795d7cea
y-wakugami <renesis2b@gmail.com>
2021-08-14 23:40:46 +0900

    コメントアウト

--- src/pages/searches/index.tsx
+++ src/pages/searches/index.tsx
@@ -1,24 +1,24 @@
 import Layout from '../../components/templates/Layout'
 import CardIndex from '../../components/organisms/CardIndex'
-import useSearchInteractor from '../../Interactors/search/useSearchInteractor'
+// import useSearchInteractor from '../../Interactors/search/useSearchInteractor'
 
 
 const SearchesIndex = () => {
-  const [response, error] = useSearchInteractor()
+  // const [response, error] = useSearchInteractor()
 
-  console.log(response)
-  console.log(error)
+  // console.log(response)
+  // console.log(error)
 
   const link = ''
   
   return (
     <Layout title='テスト'>
-      <p>検索結果</p>
-      {
-        response !== null
-          ? response.map(track => <CardIndex key={track.id} song={track} />)
-          : <p>{}</p>
-      }
+      {/*<p>検索結果</p>*/}
+      {/*{*/}
+      {/*  response !== null*/}
+      {/*    ? response.map(track => <CardIndex key={track.id} song={track} />)*/}
+      {/*    : <p>{}</p>*/}
+      {/*}*/}
     </Layout>
   )
 }
