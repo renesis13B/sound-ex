0cce5f124b5cba4026d9cff23952d3692ae3b7e2
y-wakugami <renesis2b@gmail.com>
2021-08-19 10:17:33 +0900

    Headerのデザイン修正

---
+++ src/pages/test/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '../../components/templates/Layout'
+
+const TestIndex = () => {
+  return (
+    <Layout title='SOUND EX'>
+      <p>コンポーネントテスト</p>
+    </Layout>
+  )
+}
+
+export default TestIndex
