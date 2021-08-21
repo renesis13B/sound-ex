75abda6431949291f01dbb0e25ab1927d05f71be
y-wakugami <renesis2b@gmail.com>
2021-08-22 02:36:23 +0900

    余白を追加

--- src/components/molecules/AvatarWrap.tsx
+++ src/components/molecules/AvatarWrap.tsx
@@ -6,7 +6,7 @@ type Avatar = {
 
 const AvatarWrap = ({ avatar }: Avatar) => {
   return (
-    <figure className='w-6/12 sm:w-2/12'>
+    <figure className='w-6/12 sm:w-2/12 m-2'>
       <img className='rounded-full' src={avatar.image} alt='' />
       <figcaption className='text-center'>
         {avatar.name}
