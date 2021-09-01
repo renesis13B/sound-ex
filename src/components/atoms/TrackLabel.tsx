98b20f4500da51070b2957f3e130a55ad19d8160
y-wakugami <renesis2b@gmail.com>
2021-09-01 23:30:04 +0900

    詳細画面のデザインの崩れを修正しrた

--- src/components/atoms/TrackLabel.tsx
+++ src/components/atoms/TrackLabel.tsx
@@ -1,27 +1,18 @@
+import { VFC } from 'react'
+
 type Props = {
   heading: string
   date: string | number
+  style?: string
 }
 
-const TrackLabel = ({ heading, date }: Props) => {
-  return (
-    <li className='mb-5'>
-      <dl className='flex items-center bg-white w-full max-w-md'>
-        <dt className='bg-black text-white px-5 py-3 relative mr-5'>
-          {heading}
-          <svg
-            className='absolute w-5 h-full top-0 left-full'
-            viewBox='0 0 44 43'
-            preserveAspectRatio='none'
-          >
-            <path fillRule='evenodd' fill='rgb(0, 0, 0)'
-                  d='M-0.000,43.000 L44.000,43.000 L-0.000,0.000 L-0.000,-0.000 L-0.000,43.000 Z' />
-          </svg>
-        </dt>
-        <dd className='px-5 py-3'>{`${date}`}</dd>
-      </dl>
-    </li>
-  )
-}
+const TrackLabel: VFC<Props> = ({ heading, date, style }) => (
+  <div className={`flex mb-4 ${style ?? ''}`}>
+    <dt className='w-1/2 flex items-center flex-wrap bg-black text-white px-4 py-2'>
+      {heading}
+    </dt>
+    <dd className='w-1/2 flex items-center bg-white px-4 '>{`${date}`}</dd>
+  </div>
+)
 
 export default TrackLabel
