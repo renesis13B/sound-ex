ca444b3939c2f236b93024c57d63e33152d47cf1
y-wakugami <renesis2b@gmail.com>
2021-09-09 22:30:42 +0900

    heroアイコンを導入した

--- src/components/organisms/presentational/Search.tsx
+++ src/components/organisms/presentational/Search.tsx
@@ -2,6 +2,7 @@ import React, { VFC } from 'react'
 import { SearchState, setSearch } from '../../../store/search/searchSlice'
 import { AppDispatch } from '../../../store/store'
 import { SearchSubmitHandler } from '../../../hocks/useSearchStore'
+import { SearchIcon } from '@heroicons/react/outline'
 
 type Props = {
   searchState: SearchState
@@ -37,20 +38,7 @@ const Search: VFC<Props> = ({ searchState, dispatch, submitHandler }) => {
         hover:text-black ease-in-out duration-300
         '
       >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          className='h-6 w-6'
-          fill='none'
-          viewBox='0 0 24 24'
-          stroke='currentColor'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth={2}
-            d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
-          />
-        </svg>
+        <SearchIcon className='h-6 w-6' />
       </button>
     </form>
   )
