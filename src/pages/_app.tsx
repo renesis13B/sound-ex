e2c869c20ff2ffe0140b846853c9f44add4bb9b3
y-wakugami <renesis2b@gmail.com>
2021-09-10 14:12:14 +0900

    react-query-probiderでラップする

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,28 @@ import { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import Layout from '../components/templates/Layout'
 import { store } from '../store/store'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
       <NextNprogress />
-      <Layout title='SOUND EX'>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout title='SOUND EX'>
+          <Component {...pageProps} />
+        </Layout>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   )
 }
